1e168e336d23fd4db057c9f36038f0b11b367758
Peng0809 <s4771505@student.uq.edu.au>
2025-10-02 14:27:38 +1000

    change home layout, header information and change tradeshow page

--- event-layout/src/components/Header.jsx
+++ event-layout/src/components/Header.jsx
@@ -13,10 +13,16 @@ export default function Header() {
           <NavLink to="/conference" className={link}>
             Conference
           </NavLink>
-          <NavLink to="/tradeshow" className={link}>
-            Tradeshow
+          <NavLink to="/conference-planner" className={link}>
+            Conference Planner
           </NavLink>
-          <NavLink to="/event" className={link}>
+          <NavLink to="/tradeshow" className={link}>
+            Trade Show
+          </NavLink>
+          <NavLink to="/tradeshow-planner" className={link}>
+            Trade Show Planner
+          </NavLink>
+          {/* <NavLink to="/event" className={link}>
             Event Venue
           </NavLink>
           <NavLink to="/qrcode" className={link}>
@@ -33,7 +39,7 @@ export default function Header() {
           </NavLink>
           <NavLink to="/signup" className={link}>
             Sign Up
-          </NavLink>
+          </NavLink> */}
         </nav>
       </div>
     </header>
